26a35ed0de2736e1c78d3b73b8738f1640b8357e
DilmurodZK <dilmurodzk@gmail.com>
2025-09-26 18:23:47 +0500

    last changes

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ ! -d "venv" ]; then
+    echo "🔧 Virtual environment not found. Creating one..."
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
 pip install -r requirements.txt
 pytest tests/ --maxfail=1 --disable-warnings -q
